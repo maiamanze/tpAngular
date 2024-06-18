26d9e068df0ce7753047678a7286d6dc5ae8e8e1
Maia Manze <maia.manze@gmail.com>
2024-06-18 19:46:58 -0300

    Primer componente

---
+++ src/app/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  templateUrl: './listado-peliculas.component.html',
+  styleUrl: './listado-peliculas.component.scss'
+})
+export class ListadoPeliculasComponent {
+
+}
